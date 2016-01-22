cf1dc85cef932748e38569c8129d865f29e9cfb3
DobleR19 <dobler138@gmail.com>
2016-01-22 10:08:03 +0100

    actualizacion parking

--- src/parkingraulramon/Parking.java
+++ src/parkingraulramon/Parking.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package parkingraulramon;
 
-/**
- *
- * @author daw1
- */
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
 public class Parking {
+    private String nombre;
+    private String direccion;
+    private Map<String,Plaza> listadoPlazas=new HashMap<>();
+
+    public Parking(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Map<String,Plaza> getListadoPlazas() {
+        return listadoPlazas;
+    }
+
+    public void setListadoPlazas(Map<String,Plaza> listadoPlazas) {
+        this.listadoPlazas = listadoPlazas;
+    }
     
+    public String alquilar (Vehiculo v){
+        String numPlaza=null;
+        Iterator<String> it= listadoPlazas.keySet().iterator();
+        while(it.hasNext()){
+            String snn = it.next();
+            Plaza plaza = listadoPlazas.get(snn);
+            if (plaza.getVehiculo()==null){
+                listadoPlazas.put(snn, plaza);
+                numPlaza
+            }
+        }
+        
+        return numPlaza;
+    }
 }
